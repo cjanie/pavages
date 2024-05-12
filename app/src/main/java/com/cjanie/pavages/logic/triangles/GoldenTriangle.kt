402b66d286194ceea5188c0a62d382a536a2af71
cjanie <janiechun@hotmail.fr>
2024-05-12 22:15:44 +0200

    push up sym truncated base

--- app/src/main/java/com/cjanie/pavages/logic/triangles/GoldenTriangle.kt
+++ app/src/main/java/com/cjanie/pavages/logic/triangles/GoldenTriangle.kt
@@ -214,7 +214,8 @@ class GoldenTriangle(
                     arrayOf(
                         topGoldenTriangleModel,
                         bottomGoldenTriangleModel,
-                        symBottomGoldenTriangleModel
+                        symBottomGoldenTriangleModel,
+                        symOfTopGoldenTriangleModel(topGoldenTriangleModel.goldenTriangle(), arrange)
                     ),
                     arrange
                 )
@@ -243,5 +244,24 @@ class GoldenTriangle(
     private fun getUpToKiteAndDart(iteration: Int, arrange: Boolean): Array<Decomposable> {
         return if (arrange) GoldenTriangleDecomposables_Dart_atBottom(this, iteration).decomposables()
         else GoldenTriangleDecomposables_Kite_atBottom(this).decomposables()
+    }
+
+    fun symOfTopGoldenTriangleModel(goldenTriangle: GoldenTriangle, arrange: Boolean): DecomposablesModel {
+        // define the sym golden triangle AA P1 symP1
+        // Height of A symP1 P1 = A.y - symP1.y
+        val AA = Point(
+            x = goldenTriangle.points[0].x,
+            y = goldenTriangle.symP1.y - (goldenTriangle.points[0].y - goldenTriangle.symP1.y)
+        )
+        val symGoldenTriangle_on_P1_symP1_base = GoldenTriangle(AA, goldenTriangle.P1, goldenTriangle.symP1)
+        // get the model adapter for the sym
+        val symModelAdapter = if(arrange) GoldenTriangleDecomposables_Dart_atBottom_sym(symGoldenTriangle_on_P1_symP1_base)
+        else GoldenTriangleDecomposables_Kite_atBottom_sym(symGoldenTriangle_on_P1_symP1_base)
+        // remove pyramidion // TODO a truncated base adapter
+        val symTruncatedBaseDecomposables = symModelAdapter.decomposables().toMutableList()
+            .toMutableList()
+        symTruncatedBaseDecomposables.removeAt(5)
+
+        return StateModel(symGoldenTriangle_on_P1_symP1_base, symTruncatedBaseDecomposables.toTypedArray())
     }
 }
--- app/src/main/java/com/cjanie/pavages/logic/triangles/decomposablemodels/GoldenTriangleDecomposables_RosaceUnit.kt
+++ app/src/main/java/com/cjanie/pavages/logic/triangles/decomposablemodels/GoldenTriangleDecomposables_RosaceUnit.kt
@@ -1,6 +1,7 @@
 package com.cjanie.pavages.logic.triangles.decomposablemodels
 
 import com.cjanie.pavages.logic.Decomposable
+import com.cjanie.pavages.logic.Point
 import com.cjanie.pavages.logic.triangles.GoldenTriangle
 
 class GoldenTriangleDecomposables_RosaceUnit(
@@ -24,21 +25,56 @@ class GoldenTriangleDecomposables_RosaceUnit(
             models[0].goldenTriangle().symP3,
             models[0].goldenTriangle().points[2]
         ))
+
+        val AA = Point(
+            x = models[0].goldenTriangle().points[0].x,
+            y = models[0].goldenTriangle().symP3.y - (models[0].goldenTriangle().points[0].y - models[0].goldenTriangle().symP1.y)
+        )
+        val symGoldenTriangle_on_P1_symP1_base = GoldenTriangle(AA, models[0].goldenTriangle().P1, models[0].goldenTriangle().symP1)
+        models[3].updateGoldenTriangle(symGoldenTriangle_on_P1_symP1_base)
+
         models[0].updateGoldenTriangle(
             GoldenTriangle(
             goldenTriangle.points[0],
                 models[1].goldenTriangle().points[0],
                 models[2].goldenTriangle().points[0]
-        )
-        )
+        ))
+
+
+
+
+
 
     }
+
+
+
+
     fun decomposables(): Array<Decomposable> {
-        //build()
+
         val decomposables = mutableListOf<Decomposable>()
         for (model in models) {
             decomposables.addAll(model.decomposables())
         }
         return decomposables.toTypedArray()
+    }
+
+    fun symOfTopGoldenTriangleModel(goldenTriangle: GoldenTriangle, arrange: Boolean): DecomposablesModel {
+        // define the sym golden triangle AA P1 symP1
+        // Height of A symP1 P1 = A.y - symP1.y
+        val AA = Point(
+            x = goldenTriangle.points[0].x,
+            y = goldenTriangle.symP1.y - (goldenTriangle.points[0].y - goldenTriangle.symP1.y)
+        )
+        val symGoldenTriangle_on_P1_symP1_base = GoldenTriangle(AA, goldenTriangle.P1, goldenTriangle.symP1)
+        // get the model adapter for the sym
+        val symModelAdapter = if(arrange) GoldenTriangleDecomposables_Dart_atBottom_sym(symGoldenTriangle_on_P1_symP1_base)
+        else GoldenTriangleDecomposables_Kite_atBottom_sym(symGoldenTriangle_on_P1_symP1_base)
+        // remove pyramidion // TODO a truncated base adapter
+        val symTruncatedBaseDecomposables = symModelAdapter.decomposables().toMutableList()
+            .toMutableList()
+        symTruncatedBaseDecomposables.removeAt(5)
+
+        return StateModel(symGoldenTriangle_on_P1_symP1_base, symTruncatedBaseDecomposables.toTypedArray())
     }
 }
