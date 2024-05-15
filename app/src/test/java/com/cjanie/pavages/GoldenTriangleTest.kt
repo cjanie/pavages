79bb535be3b71a5998956164ba85006d5acad03e
cjanie <janiechun@hotmail.fr>
2024-05-15 15:12:36 +0200

    iteration 3 tests

--- app/src/test/java/com/cjanie/pavages/GoldenTriangleTest.kt
+++ app/src/test/java/com/cjanie/pavages/GoldenTriangleTest.kt
@@ -1,20 +1,19 @@
 package com.cjanie.pavages
 
 import com.cjanie.pavages.logic.Cercle
+import com.cjanie.pavages.logic.Decomposable
 import com.cjanie.pavages.logic.Number
 import com.cjanie.pavages.logic.PentagonBuilder
 import com.cjanie.pavages.logic.triangles.GoldenTriangle
 import com.cjanie.pavages.logic.Point
 import com.cjanie.pavages.logic.triangles.GoldenGnomon
+import com.cjanie.pavages.logic.triangles.IsoscelesTriangle
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
-import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class GoldenTriangleTest {
-
-    val goldenTriangle = GoldenTriangle()
 
     @Test(expected = AssertionError::class)
     fun `duplicated side length is in the golden ratio phi to the base side`() {
@@ -40,60 +39,167 @@ class GoldenTriangleTest {
 
     @Test
     fun `decompose 1st iteration returns 2 golden triangles and 1 golden gnomon`() {
-        val goldenTriangle = GoldenTriangle()
-        val decomposables = goldenTriangle.iterate(1)
-        assertEquals(3, decomposables.size)
+        // SUT
+        val decomposables = GoldenTriangle().iterate(1)
 
-        val goldenTriangles = decomposables.filter { it is GoldenTriangle }
-        assertEquals(2, goldenTriangles.size)
-
-        val gnomons = decomposables.filter { it is GoldenGnomon }
-        assertEquals(1, gnomons.size)
+        assertDecompositionRatio(
+            expectedTotal = 3,
+            expectedGoldenTrianglesCount = 2,
+            expectedGoldenGnomonsCount = 1,
+            decomposables
+            )
     }
 
     @Test fun `decompose 1st iteration non adjacent golden triangles arrangement`() {
         // SUT
         val decomposables = GoldenTriangle().iterate(1)
-        val goldenTriangles = decomposables.filter { it is GoldenTriangle }
-        val isAdjacent: Boolean = (goldenTriangles[0] as GoldenTriangle).points[0] == (goldenTriangles[1] as GoldenTriangle).points[0]
-        assertFalse(isAdjacent)
+
+        assertFalse(isAdjacentGoldenTriangles(decomposables))
     }
 
     @Test
-    fun `decompose 1st iteration another arrangement`() {
+    fun `decompose 1st iteration adjacent golden triangles arrangement`() {
 
-        val decomposables = goldenTriangle.iterate(1, true)
-        assertEquals(3, decomposables.size)
+        val decomposables = GoldenTriangle().iterate(1, true)
 
-        val goldenTriangles = decomposables.filter { it is GoldenTriangle }
-        assertEquals(2, goldenTriangles.size)
+        assertDecompositionRatio(
+            expectedTotal = 3,
+            expectedGoldenTrianglesCount = 2,
+            expectedGoldenGnomonsCount = 1,
+            decomposables
+        )
 
-        val gnomons = decomposables.filter { it is GoldenGnomon }
-        assertEquals(1, gnomons.size)
+        assertTrue(isAdjacentGoldenTriangles(decomposables))
     }
 
     @Test
     fun `decompose 2nd iteration return 5 golden triangles and 3 golden gnomons`() {
-        val decomposables = goldenTriangle.iterate(2)
-        assertEquals(8, decomposables.size)
 
-        val goldenTriangles = decomposables.filter { it is GoldenTriangle }
-        assertEquals(5, goldenTriangles.size)
+        val decomposables = GoldenTriangle().iterate(2)
 
-        val gnomons = decomposables.filter { it is GoldenGnomon }
-        assertEquals(3, gnomons.size)
+        assertDecompositionRatio(
+            expectedTotal = 8,
+            expectedGoldenTrianglesCount = 5,
+            expectedGoldenGnomonsCount = 3,
+            decomposables
+        )
     }
 
     @Test
-    fun `decompose 2nd other arrangement`() {
-        val decomposables = goldenTriangle.iterate(2, true)
-        assertEquals(8, decomposables.size)
+    fun `decompose 2nd iteration non adjacent golden triangles arrangement`() {
 
-        val goldenTriangles = decomposables.filter { it is GoldenTriangle }
-        assertEquals(5, goldenTriangles.size)
+        val decomposables = GoldenTriangle().iterate(2)
 
-        val gnomons = decomposables.filter { it is GoldenGnomon }
-        assertEquals(3, gnomons.size)
+        assertFalse(isAdjacentGoldenTriangles(decomposables))
+    }
+
+    @Test
+    fun `decompose 2nd adjacent golden triangles arrangement`() {
+
+        val decomposables = GoldenTriangle().iterate(2, true)
+
+        assertDecompositionRatio(
+            expectedTotal = 8,
+            expectedGoldenTrianglesCount = 5,
+            expectedGoldenGnomonsCount = 3,
+            decomposables
+        )
+
+        assertTrue(isAdjacentGoldenTriangles(decomposables))
+    }
+
+    @Test
+    fun `decompose third iteration returns 13 golden triangles and 8 golden gnomons`() {
+
+        val decomposables = GoldenTriangle().iterate(3, false)
+
+        assertDecompositionRatio(
+            expectedTotal = 21,
+            expectedGoldenTrianglesCount = 13,
+            expectedGoldenGnomonsCount = 8,
+            decomposables
+        )
+    }
+
+    @Test
+    fun `decompose third iteration 5 adjacent golden triangles in P1 arrangement`() {
+        val goldenTriangle = GoldenTriangle()
+
+        val decomposables = goldenTriangle.iterate(3, false)
+
+        assertDecompositionRatio(
+            expectedTotal = 21,
+            expectedGoldenTrianglesCount = 13,
+            expectedGoldenGnomonsCount = 8,
+            decomposables
+        )
+
+        val goldenTriangles: Array<GoldenTriangle> = (decomposables.filter { it is GoldenTriangle } as List<GoldenTriangle>).toTypedArray()
+        val adjacentsInP1 = goldenTriangles.filter { it.points[0] == goldenTriangle.P1 }
+
+        assertEquals(5, adjacentsInP1.size)
+
+        val kiteInB = goldenTriangles.filter { it.points[0] == goldenTriangle.points[1] }
+        assertEquals(2, kiteInB.size)
+
+        val topNonAdjacent = goldenTriangles.filter {it.points[0] == goldenTriangle.P2}
+        assertEquals(0, topNonAdjacent.size)
+    }
+
+    @Test
+    fun `decompose third iteration 5 adjacent golden gnomons in P1 arrangement`() {
+        val goldenTriangle = GoldenTriangle()
+
+        val decomposables = goldenTriangle.iterate(3, true)
+
+        assertDecompositionRatio(
+            expectedTotal = 21,
+            expectedGoldenTrianglesCount = 13,
+            expectedGoldenGnomonsCount = 8,
+            decomposables
+        )
+
+        val goldenGnomons: Array<GoldenGnomon> = (decomposables.filter { it is GoldenGnomon } as List<GoldenGnomon>).toTypedArray()
+        val adjacentsInP1ByBasePoint1 = goldenGnomons.filter { it.points[1] == goldenTriangle.P1 }
+        val adjacentsInP1ByBasePoint2 = goldenGnomons.filter { it.points[2] == goldenTriangle.P1 }
+        val adjacents = mutableListOf<GoldenGnomon>()
+        adjacents.addAll(adjacentsInP1ByBasePoint1)
+        adjacents.addAll(adjacentsInP1ByBasePoint2)
+        assertEquals(5, adjacents.size)
+
+        val goldenTriangles: Array<GoldenTriangle> = (decomposables.filter { it is GoldenTriangle } as List<GoldenTriangle>).toTypedArray()
+        val kiteInB = goldenTriangles.filter { it.points[0] == goldenTriangle.points[1] }
+        assertEquals(2, kiteInB.size)
+
+        val topNonAdjacent = goldenTriangles.filter {it.points[0] == goldenTriangle.P2}
+        assertEquals(0, topNonAdjacent.size)
+    }
+
+
+
+    private fun assertDecompositionRatio(
+        expectedTotal: Int,
+        expectedGoldenTrianglesCount: Int,
+        expectedGoldenGnomonsCount: Int,
+        actualDecomposables: Array<Decomposable>) {
+        assertEquals(expectedTotal, actualDecomposables.size)
+
+        val goldenTriangles = actualDecomposables.filter { it is GoldenTriangle }
+        assertEquals(expectedGoldenTrianglesCount, goldenTriangles.size)
+
+        val goldenGnomons = actualDecomposables.filter { it is GoldenGnomon }
+        assertEquals(expectedGoldenGnomonsCount, goldenGnomons.size)
+    }
+
+    private fun isAdjacentGoldenTriangles(decomposables: Array<Decomposable>) : Boolean {
+        val goldenTriangles = (decomposables.filter { it is GoldenTriangle } as List<IsoscelesTriangle>).toTypedArray()
+        return isAdjacent(goldenTriangles)
+
+    }
+
+    private fun isAdjacent(isocèles: Array<IsoscelesTriangle>): Boolean {
+        val oppositeToBasePoint = isocèles[0].points[0]
+        return isocèles.size == isocèles.filter { it.points[0] == oppositeToBasePoint}.size
     }
 
 }
